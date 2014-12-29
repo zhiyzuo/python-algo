fd82cb93c44bbca5afb97ef192d87606e5bd7957
Zhiya Zuo <zhiyazuo@gmail.com>
2014-12-28 19:50:01 -0600

    add counting sort;modify .gitignore

--- sort.py
+++ sort.py
@@ -113,3 +113,27 @@ def quickSort(l):
     right = quickSort(l[i+1:])
 
     return left + [l[i]] + right
+
+def countingSort(l, keyRange=None):
+    """ Counting sort for list/array (discrete&finite sorting keys)
+        Return a new list that is sorted
+    """
+
+    if keyRange == None:
+        keyRange = max(l) + 1
+    countList = [0]*keyRange
+    accumulateList = [0]*keyRange
+
+    for key in l:
+        countList[key] = countList[key] + 1
+    for i in range(1, keyRange):
+        accumulateList[i] = countList[i-1] + accumulateList[i-1]
+
+    sorted_l = [None]*len(l)
+    for j in range(len(l)):
+        key = l[j]
+        index = accumulateList[key]
+        sorted_l[index] = key
+        accumulateList[key] = accumulateList[key] + 1
+
+    return sorted_l
