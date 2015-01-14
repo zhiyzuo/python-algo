971cf6c3bf1760a0070158aad0c0be3cd8347f31
Zhiya Zuo <zhiyazuo@gmail.com>
2015-01-14 15:53:57 -0600

    add directed graph class; TODO: better manage edge list in graph and digraph

--- graph.py
+++ graph.py
@@ -1,17 +1,31 @@
 class Node:
+# {{{ Node Class
+    nodeID = 0
     def __init__(self, name):
         self.name = name
+        self.id = Node.nodeID
+        Node.nodeID = Node.nodeID + 1
+
+    def getID(self):
+        return self.id
 
     def getName(self):
         return self.name
 
+    def copy(self):
+        return Node(self.name)
+
     def __str__(self):
-        return '<Node {}>'.format(self.name)
+        return '<Node: id {} name {}>'.format(self.id, self.name)
 
     def __repr__(self):
-        return '<Node {}>'.format(self.name)
+        return self.__str__()
+# }}}
 
 class Edge:
+# {{{ Edge Class
+    EdgeID = 0
+
     def __init__(self, source, target, weight=1):
     # both source and target are Node objects
         self.source = source
@@ -21,6 +35,12 @@ class Edge:
         else:
             self.weight = 1 # default
             print "Please enter an integer for weight! (Now set to default value 1)"
+
+        self.id = Edge.EdgeID
+        Edge.EdgeID = Edge.EdgeID + 1
+
+    def getID(self):
+        return self.id
 
     def getSource(self):
         return self.source
@@ -37,15 +57,25 @@ class Edge:
         else:
             self.weight = 1 # default
             print "Please enter an integer for weight! (Now set to default value 1)"
+    
+    def reverse(self):
+        source = self.source
+        target = self.target
+        weight = self.weight
+        return Edge(target, source, weight)
 
     def __str__(self):
-        return '<Edge ({},{}) with weight {}>'.format(self.source.getName(), self.target.getName(), self.getWeight())
+        return '<Edge id {} ({} -> {}) with weight {}>'.format(self.id, self.source.getName(), self.target.getName(), self.getWeight())
 
     def __repr__(self):
         return self.__str__()
+# }}}
 
 # undirected graph
 class Graph:
+# {{{ Graph Class
+
+    GraphID = 0
     def __init__(self, nodes=[], edges=[]):
     # nodes: list of Node objects;
     # edges: list of Edge objects;
@@ -57,8 +87,14 @@ class Graph:
 
         if type(edges) == list:
             self.edges = edges
+            reversedEdges = []
+            for edge in self.edges:
+                reversedEdge = edge.reverse()
+                reversedEdges.append(reversedEdge)
+            self.edges.extend(reversedEdges)
+
         else:
-            self.edges = [edges]
+            self.edges = [edges, edges.reverse()]
 
         self.adjList = {}
         for edge in self.edges:
@@ -67,10 +103,18 @@ class Graph:
             weight = edge.getWeight()
             if source in self.nodes and target in self.nodes:
                 # no directions: add twice
-                self.adjList[source] = (target, weight)
-                self.adjList[target] = (source, weight)
+                if source in self.adjList:
+                    self.adjList[source].append((target, weight))
+                else:
+                    self.adjList[source] = [(target, weight)]
             else:
                 print "source or target node is not eligible!"
+
+        self.id = Graph.GraphID
+        Graph.GraphID = Graph.GraphID + 1
+
+    def getID(self):
+        return self.id
 
     def getAdjList(self):
         return self.adjList
@@ -110,9 +154,15 @@ class Graph:
         weight = edge.getWeight()
         if source in self.nodes and target in self.nodes:
             # no directions: add twice
-            self.edges.append(edge)
-            self.adjList[source] = (target, weight)
-            self.adjList[target] = (source, weight)
+            if source in self.adjList:
+                self.adjList[source].append((target, weight))
+            else:
+                self.adjList[source] = [(target, weight)]
+
+            if target in self.adjList:
+                self.adjList[target].append((source, weight))
+            else:
+                self.adjList[target] = [(source, weight)]
         else:
             print "source or target node is not eligible!"
 
@@ -121,10 +171,10 @@ class Graph:
             self.addEdge(edge)
 
     def removeEdge(self, edge):
-        if self.hadEdge(edge):
+        if self.hasEdge(edge):
             self.edges.remove(edge)
-            del self.adjList[edge.getSource]
-            del self.adjList[edge.getTarget]
+            del self.adjList[edge.getSource()]
+            del self.adjList[edge.getTarget()]
         else:
             print "{} is not in this graph!".format(edge)
 
@@ -133,10 +183,57 @@ class Graph:
             self.removeEdge(edge)
 
     def __str__(self):
-        string = '\nGraph with\nnodes: {}\nedges:  '.format(self.nodes)
+        string = '\nGraph id {} with\nnodes: {}\nedges:  '.format(self.id, self.nodes)
         for edge in self.edges:
             string += edge.__repr__() + '\n\t'  
         return string
 
     def __repr__(self):
         return self.__str__()
+# }}}
+
+class DiGraph(Graph):
+# {{{ Directed Graph Class
+
+    DiGraphID = 0
+
+    def __init__(self, nodes=[], edges=[]):
+    # nodes: list of Node objects;
+    # edges: list of Edge objects;
+
+        if type(nodes) == list:
+            self.nodes = nodes
+        else:
+            self.nodes = [nodes]
+
+        if type(edges) == list:
+            self.edges = edges
+        else:
+            self.edges = [edges]
+
+        self.adjList = {}
+        for edge in self.edges:
+            source = edge.getSource()
+            target = edge.getTarget()
+            weight = edge.getWeight()
+            if source in self.nodes and target in self.nodes:
+                # with directions: add once
+                if source in self.adjList:
+                    self.adjList[source].append((target, weight))
+                else:
+                    self.adjList[source] = [(target, weight)]
+            else:
+                print "source or target node is not eligible!"
+
+        self.id = DiGraph.DiGraphID
+        DiGraph.DiGraphID = DiGraph.DiGraphID + 1
+
+    def __str__(self):
+        string = '\nDirected Graph id {} with\nnodes: {}\nedges:  '.format(self.id, self.nodes)
+        for edge in self.edges:
+            string += edge.__repr__() + '\n\t'  
+        return string
+
+    def __repr__(self):
+        return self.__str__()
+# }}}
